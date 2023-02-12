215178ddda4fbf1296e0dbf6d46f400f2845d327
MouncetCodeUniverse <123984846+MouncetCodeUniverse@users.noreply.github.com>
2023-02-12 20:06:00 +0100

    Lesson 5 from c# course by CodeUniverse team

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Lesson 5 الدرس الخامس
+// comparison and logical operators
+// عمليات المقارنة والعمليات المنطقية
+
+// comparison operators عمليات المقارنة
+System.Console.WriteLine(3 < 6);
+System.Console.WriteLine(3 > 6);
+System.Console.WriteLine(   6<=6);
+System.Console.WriteLine(   6>=6);
+System.Console.WriteLine(   7 == 7);
+System.Console.WriteLine(   5 != 4);
+
+// Logical operators العمليات المنطقية
+System.Console.WriteLine(3 < 6 && 4 < 7);
+System.Console.WriteLine(3 < 6 && 4 > 7);
+System.Console.WriteLine(3 < 6 || 4 > 7);
+System.Console.WriteLine(!(2 == 2));
+
+
+
+
+
